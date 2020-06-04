60144d7e1a74b8fc16819787a396a822f8c6a967
Ala Douagi <alado@xtech.guru>
2020-06-04 17:03:17 +0100

    Code generated after amplify push

---
+++ src/graphql/queries.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getCoins = /* GraphQL */ `
+  query GetCoins($limit: Int, $start: Int) {
+    getCoins(limit: $limit, start: $start) {
+      id
+      name
+      symbol
+      price_usd
+    }
+  }
+`;
