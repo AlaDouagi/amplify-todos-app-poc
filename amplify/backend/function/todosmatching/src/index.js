868908deb6aa916e5b60ecacb3137497403b8cd4
Ala Douagi <alado@xtech.guru>
2020-06-04 16:51:27 +0100

    Add new Amplify lambda function (todosmatching)
    * This function includes some code pasted from https://dev.to/dabit3/lambda-function-graphql-resolvers-11cd

---
+++ amplify/backend/function/todosmatching/src/index.js
@@ -0,0 +1,16 @@
+// amplify/backend/function/todosmatching/src/index.js
+const axios = require('axios');
+
+exports.handler = function (event, _, callback) {
+  let apiUrl = `https://api.coinlore.com/api/tickers/?start=1&limit=10`;
+
+  if (event.arguments) {
+    const { start = 0, limit = 10 } = event.arguments;
+    apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`;
+  }
+
+  axios
+    .get(apiUrl)
+    .then((response) => callback(null, response.data.data))
+    .catch((err) => callback(err));
+};
