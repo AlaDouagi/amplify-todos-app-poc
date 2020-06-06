e13a900f59c477d4ce7409f71a64fc0a0c1fc69c
Ala Douagi <alado@xtech.guru>
2020-06-07 00:44:58 +0100

    Remove 'todosmatching' Lambda function and use newly created 'matchtodos' Amazon DynamoDB Stream

---
+++ amplify/backend/function/matchtodos/src/index.js
@@ -0,0 +1,42 @@
+// API_AMPLIFYTODOSAPPPOCAPI_GRAPHQLAPIIDOUTPUT
+// API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_ARN
+// API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_NAME
+// ENV
+// REGION
+
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// check https://www.gravitywell.co.uk/insights/how-to-use-your-mocked-dynamodb-with-appsync-and-lambda/
+// Mock Mode
+const isFake = process.env.ENV === 'NONE';
+
+const region = isFake ? 'us-fake-1' : process.env.REGION;
+
+const awsConfig = {
+  region,
+};
+
+if (isFake) {
+  awsConfig.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  awsConfig.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  awsConfig.endpoint = process.env.DYNAMODB_ENDPOINT;
+}
+
+const docClient = new AWS.DynamoDB.DocumentClient(awsConfig);
+
+const params = {
+  TableName: process.env.API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_NAME,
+};
+
+exports.handler = function (event, context, callback) {
+  docClient.scan(params, function (err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data.Items);
+    }
+  });
+};
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,17 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getTodos = /* GraphQL */ `
-  query GetTodos($limit: Int, $start: Int) {
-    getTodos(limit: $limit, start: $start) {
-      id
-      title
-      description
-      updatedAt
-      createdAt
-    }
-  }
-`;
 export const getMatchingTodos = /* GraphQL */ `
   query GetMatchingTodos($name: String!) {
     getMatchingTodos(name: $name) {
