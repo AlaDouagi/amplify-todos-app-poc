a450d06708254fdae56cb52c9b3eade7bc9a3ce3
Ala Douagi <alado@xtech.guru>
2020-06-07 02:03:57 +0100

    Add auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
