325b5d19cad376df76326db13d124741c998980f
Ala Douagi <alado@xtech.guru>
2020-06-07 21:13:27 +0100

    Add deleteAllTodos mutation that invokes deletealltodos lambda resolver

---
+++ amplify/backend/function/deletealltodos/src/index.js
@@ -0,0 +1,71 @@
+/* Amplify Params - DO NOT EDIT
+	API_AMPLIFYTODOSAPPPOCAPI_GRAPHQLAPIIDOUTPUT
+	API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_ARN
+	API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_NAME
+	ENV
+  REGION
+*/
+
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// check https://www.gravitywell.co.uk/insights/how-to-use-your-mocked-dynamodb-with-appsync-and-lambda/
+// Mock Mode
+const isFake = process.env.ENV === 'NONE';
+
+const region = isFake ? 'us-fake-1' : process.env.REGION;
+
+const awsConfig = {
+  region,
+};
+
+if (isFake) {
+  awsConfig.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  awsConfig.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  awsConfig.endpoint = process.env.DYNAMODB_ENDPOINT;
+}
+
+const docClient = new AWS.DynamoDB.DocumentClient(awsConfig);
+
+const hashKey = 'id';
+const rangeKey = null;
+
+exports.handler = function (event, context, callback) {
+  const scanParams = {
+    TableName: process.env.API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_NAME,
+  };
+
+  docClient.scan(scanParams, function (err, data) {
+    if (err) callback(err);
+    // an error occurred
+    else {
+      data.Items.forEach(function (obj, i) {
+        const params = {
+          TableName: scanParams.TableName,
+          Key: buildKey(obj),
+          ReturnValues: 'NONE', // optional (NONE | ALL_OLD)
+          ReturnConsumedCapacity: 'NONE', // optional (NONE | TOTAL | INDEXES)
+          ReturnItemCollectionMetrics: 'NONE', // optional (NONE | SIZE)
+        };
+
+        docClient.delete(params, function (err, dataItem) {
+          if (err) callback(err);
+          // an error occurred
+          else if (i === data.Items.length - 1) callback(null, data.Items); // successful response
+        });
+      });
+    }
+  });
+};
+
+function buildKey(obj) {
+  const key = {};
+  key[hashKey] = obj[hashKey];
+  if (rangeKey) {
+    key[rangeKey] = obj[rangeKey];
+  }
+
+  return key;
+}
