0412f2ac2e73c8bfe20b15509234855fd3130b02
Ala Douagi <alado@xtech.guru>
2020-06-08 15:35:40 +0100

    Fix matchtodos failing when there is no todos for the owner

--- amplify/backend/function/matchtodos/src/index.js
+++ amplify/backend/function/matchtodos/src/index.js
@@ -45,36 +45,40 @@ exports.handler = function (event, context, callback) {
     if (err) {
       callback(err);
     } else {
-      const titleValues = data.Items.map(({ title }) => title);
-      const titleObject = {};
+      if ((data.Items || []).length === 0) {
+        callback(null, []);
+      } else {
+        const titleValues = data.Items.map(({ title }) => title);
+        const titleObject = {};
 
-      titleValues.forEach(function (value, index) {
-        const titleKey = ':titlevalue' + index;
-        titleObject[titleKey.toString()] = value;
-      });
+        titleValues.forEach(function (value, index) {
+          const titleKey = ':titlevalue' + index;
+          titleObject[titleKey.toString()] = value;
+        });
 
-      const matchedTodosParams = {
-        TableName: process.env.API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_NAME,
-        FilterExpression:
-          '#owner_id <> :owner AND title IN (' +
-          Object.keys(titleObject).toString() +
-          ')',
-        ExpressionAttributeValues: {
-          ...titleObject,
-          ':owner': event.arguments.owner,
-        },
-        ExpressionAttributeNames: {
-          '#owner_id': 'owner',
-        },
-      };
+        const matchedTodosParams = {
+          TableName: process.env.API_AMPLIFYTODOSAPPPOCAPI_TODOTABLE_NAME,
+          FilterExpression:
+            '#owner_id <> :owner AND title IN (' +
+            Object.keys(titleObject).toString() +
+            ')',
+          ExpressionAttributeValues: {
+            ...titleObject,
+            ':owner': event.arguments.owner,
+          },
+          ExpressionAttributeNames: {
+            '#owner_id': 'owner',
+          },
+        };
 
-      docClient.scan(matchedTodosParams, function (err, data) {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null, data.Items);
-        }
-      });
+        docClient.scan(matchedTodosParams, function (err, data) {
+          if (err) {
+            callback(err);
+          } else {
+            callback(null, data.Items);
+          }
+        });
+      }
     }
   });
 };
